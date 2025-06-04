cd334a8954d4579fcfcb6bc99d8104a762bcee0f
Marcel Mueller <marcel.mueller@thch.uni-bonn.de>
2025-06-04 12:34:11 +0200

    bump version to 3.0.0
    Signed-off-by: Marcel Mueller <marcel.mueller@thch.uni-bonn.de>

--- app/main.f90
+++ app/main.f90
@@ -61,7 +61,7 @@ program main
    cm                = "ceh"
 
    !##### VERSION STRING #####
-   version = "2.3.0"
+   version = "3.0.0"
    !##########################
 
    ! get number of arguments
